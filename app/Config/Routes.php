aff3a57026187e164af96abc69328444617ba72e
JexzX <jexzx7@gmail.com>
2025-06-25 00:22:48 +0700

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,24 @@
 <?php
 
+namespace Config;
+
 use CodeIgniter\Router\RouteCollection;
 
-/**
- * @var RouteCollection $routes
- */
-$routes->get('/', 'Home::index');
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Pages');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+$routes->setAutoRoute(false);
+
+// Main Routes
+$routes->get('/', 'Pages::index');
+$routes->get('home', 'Pages::index');
+$routes->get('about', 'Pages::index/about');
+$routes->get('services', 'Pages::index/services');
+$routes->match(['get', 'post'], 'contact', 'Pages::contact');
+$routes->get('(:any)', 'Pages::index/$1');
+
+if (is_file(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
+    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
+}

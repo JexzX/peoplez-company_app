374c02576fa8099d26cf6a7f3954c0e7b453372f
JexzX <jexzx7@gmail.com>
2025-06-24 23:23:59 +0700

    basic-upload

---
+++ app/Database/Migrations/2023-01-01-000000_CreatePagesTable.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePagesTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'slug' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'unique' => true,
+            ],
+            'content' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'meta_description' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addKey('slug');
+        $this->forge->createTable('pages');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('pages');
+    }
+}
